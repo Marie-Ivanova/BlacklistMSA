66a81159382a109a18606b3422d8fda3d9754344
Marie-Ivanova <athenyx66@gmail.com>
2026-09-07 19:15:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // À REMPLIR après création du projet Supabase.
 // Exemple :
 // window.SUPABASE_CONFIG = {
-//   url: "https://xxxxxxxx.supabase.co",
+//   url: "https://xjzuzrzcluweaxxmddki.supabase.co",
 //   anonKey: "eyJ..."
 // };
 window.SUPABASE_CONFIG = {
   url: "",
-  anonKey: ""
+  anonKey: "sb_publishable_1D4LsK0ClUzbMMghK9U1hw_sVEN2zBg"
 };
